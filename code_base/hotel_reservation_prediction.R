a0dda121f90b956d1474848a4412566753b72507
saianjesh <saianjesh@aol.com>
2024-04-20 21:09:38 -0500

    updated libraries

--- code_base/hotel_reservation_prediction.R
+++ code_base/hotel_reservation_prediction.R
@@ -3,3 +3,26 @@
 # Instead of Rmd
 # Will transfer the code blocks to Rmd
 ##############################################
+
+## Importing Necessary Libraries
+##---------
+library(dplyr)
+library(caret)
+library(mltools)
+library(data.table)
+##---------
+# library(tm)
+# library(class)
+# library(gmodels)
+# library(wordcloud)
+# library(SnowballC)
+# library(e1071)
+# library(qdap)
+# library(C50)
+# library(abind)
+# library(tfruns)
+# library(keras)
+
+
+
+## Reading dataset
